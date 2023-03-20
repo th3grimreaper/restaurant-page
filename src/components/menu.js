f2af2578193f8267be87083b194e2f2c9192e98e
reaper <saip589@gmail.com>
2023-03-21 00:29:41 +0530

    add menu items

---
+++ src/components/menu.js
@@ -0,0 +1,32 @@
+import indoripoha from '../assets/indoripoha.jpg'
+
+const content = document.querySelector('.content')
+
+function clearContent() {
+  content.innerHTML = ''
+}
+
+function menuPage() {
+  clearContent()
+  const cardContainer = document.createElement('section')
+  cardContainer.classList.add('card-container')
+  const textWrapper = document.createElement('div')
+  textWrapper.classList.add('text-wrapper')
+  const title = document.createElement('div')
+  title.textContent = 'Indori Poha'
+  const desc = document.createElement('div')
+  desc.textContent =
+    'One of the popular street food snacks from Indore is poha with Jalebi. the poha which is made in Indore is very different than the Maharashtrian onion Poha recipe. In Indori poha onions are not sautéed but added as a topping on the poha. Along with onions some more ingredients are added as toppings. The taste of Indori poha is khatta meetha meaning sweet and sour. Fennel seeds are also added which give the poha a floral aroma and sweetish tones.'
+  const imgWrap = document.createElement('div')
+  imgWrap.classList.add('img-wrapper')
+  const img = document.createElement('img')
+  img.src = indoripoha
+  imgWrap.appendChild(img)
+  textWrapper.appendChild(title)
+  textWrapper.appendChild(desc)
+  cardContainer.appendChild(textWrapper)
+  cardContainer.appendChild(imgWrap)
+  content.appendChild(cardContainer)
+}
+
+export default menuPage
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import homePage from './components/home'
+import menuPage from './components/menu'
+import aboutPage from './components/about'
 import './styles/main.scss'
+
+const navArray = document.querySelectorAll('a')
+
+function switchPage(e) {
+  if (e.target.id === 'menu') {
+    menuPage()
+  } else if (e.target.id === 'about') {
+    aboutPage()
+  } else {
+    homePage()
+  }
+}
+
+navArray.forEach((value) => {
+  value.addEventListener('click', switchPage)
+})
